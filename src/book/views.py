f2726c150408baf80e9a1fe078d8bf0e4f806f79
Ваше Имя <serg148>
2020-10-18 09:28:50 +0300

    flake8 + travis

--- src/book/views.py
+++ src/book/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from book.models import Book
+
 from django.http import HttpResponse
+
 from faker import Faker
 
-from book.models import Book
 
 # Create your views here.r
 def books_list(request):
@@ -12,15 +14,11 @@ def books_list(request):
         results += f' id: {book.id} title: {book.title} author: {book.author},'
     return HttpResponse(results)
 
+
 def books_create(request):
     fake = Faker()
     book = Book.objects.create(
-        author = fake.name(),
-        title = fake.paragraph(nb_sentences=1)
+        author=fake.name(),
+        title=fake.paragraph(nb_sentences=1)
     )
     return HttpResponse(f"Book created!  |  Title: {book.title}  |  Author: {book.author}")
-
-
-
-
-
